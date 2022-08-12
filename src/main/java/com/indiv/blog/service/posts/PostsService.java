d354d2551ff9fd3e77866f77b199d4f5db74c5b6
michael <mskim_451@dongguk.edu>
2022-08-12 16:49:13 +0900

    feature : 구글 서비스 등록

--- src/main/java/com/indiv/blog/service/posts/PostsService.java
+++ src/main/java/com/indiv/blog/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.indiv.blog.service.posts;
 
 import com.indiv.blog.domain.posts.Posts;
 import com.indiv.blog.domain.posts.PostsRepository;
+import com.indiv.blog.web.dto.PostsListResponseDto;
 import com.indiv.blog.web.dto.PostsResponseDto;
 import com.indiv.blog.web.dto.PostsSaveRequestDto;
 import com.indiv.blog.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +42,12 @@ public class PostsService {
     public void delete(Long id) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
