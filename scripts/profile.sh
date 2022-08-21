bbf6c4f1a26b5199a3d6af403319a2956e825977
michael <mskim_451@dongguk.edu>
2022-08-21 17:38:42 +0900

    -
    -

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# 쉬고 있는 profile 찾기: real1이 사용주잉면 real2가 쉬고 있고, 반대면 real1이 쉬고 이씅ㅁ
+# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 
 function find_idle_profile() {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
@@ -26,7 +26,7 @@ function find_idle_profile() {
 function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1]
+    if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
     else
